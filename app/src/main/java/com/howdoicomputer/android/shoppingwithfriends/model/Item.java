25cd3dfece323325278100e258e5e25fabf479dc
rmacias3 <rmacias3@gatech.edu>
2015-02-24 16:55:25 -0500

    implemented comparable in item

--- app/src/main/java/com/howdoicomputer/android/shoppingwithfriends/model/Item.java
+++ app/src/main/java/com/howdoicomputer/android/shoppingwithfriends/model/Item.java
@@ -11,6 +11,7 @@ import java.util.Date;
  * Work in Progress
  */
 public class Item implements Comparable {
+    //instance variables
     private int likes;
     private double price;
     private Location location;
